333840d71aefe8596486f9050537c59c71579a36
Stanislav Turcuman <turcuman.stanislav@gmail.com>
2024-01-03 16:08:46 +0200

    Remove forName method from getConnection

--- JRResultSetDataSource/src/main/java/md/turcuman/jasper/jrresultset/Utils/DatabaseConnector.java
+++ JRResultSetDataSource/src/main/java/md/turcuman/jasper/jrresultset/Utils/DatabaseConnector.java
@@ -10,13 +10,12 @@ public class DatabaseConnector {
 
     public static Connection getConnection(Properties properties, Logger logger) {
         try {
-            Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection(
                     properties.getProperty("db.url"),
                     properties.getProperty("db.username"),
                     properties.getProperty("db.password")
             );
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             logger.severe("Error while connecting to DB: " + e.getMessage());
             return null;
         }
